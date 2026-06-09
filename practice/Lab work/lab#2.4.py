9bdd04e1ba17cea103b178ce82dd022f074524fb
krish <krishdelvadiya34@gmail.com>
2026-06-09 12:21:02 +0530

    logicbox

---
+++ practice/Lab work/lab#2.4.py
@@ -0,0 +1,23 @@
+#question 1:
+
+for i in range(1,21):
+    if i%4==0:
+        continue
+    print(i)
+
+#question 2:
+
+i=0
+
+while i<=10:
+    if i==7:
+        break
+    i+=1
+    print(i)
+
+#question 3:
+
+i="python"
+
+
+
---
+++ projects/PR-2/logicbox.py
@@ -0,0 +1,80 @@
+print("welcome to the pattern generator and number analyzer!")
+
+while True:
+    print("\nSelect an Option:")
+    print("\n1. Generate a Pattern")
+    print("2. Analyze a Range of Number")
+    print("3. Exit")
+   
+
+    choice=int(input("\nEnter your choice:"))
+
+    match choice:
+        case 1:
+            print("\nEnter 1 for {*} pattern")
+            print("Enter 2 for {1,2,3} pattern")
+
+            choice=int(input("\nEnter your choice:"))
+
+            if choice==1:
+             
+                raw=int(input("\nEnter the number of raw for the pattern:"))
+                print("\nThis is {*} pattern:")
+                for i in range(1,raw):
+                  for j in range(i):
+                    print("*",end=(""))
+                  print()
+        
+                for i in range(raw,1,-1):
+                  for j in range(i):
+                    print("*",end=(""))
+                  print()
+
+            if choice==2:
+               
+                raw=int(input("\nEnter the number of raw for the pattern:"))
+                print("\nThis is {1,2,3} pattern:")
+                for i in range(1,raw+1):
+                  for j in range(i):
+                    print(i,end=(""))
+                  print()
+
+
+
+        case 2:
+            sum=0
+            num1=int(input("\nEnter the start of the range:"))
+            num2=int(input("Enter the end of the range:"))
+
+            
+            if num1>num2:
+                for i in range(num1,num2-1,-1):
+                    if i%2==0:
+                        print(i,"is Even")
+                    elif i%2!=0:
+                        print(i,"is Odd")
+                    elif i==0:
+                        print(i,"is Neutral")
+                    else:
+                        print("Invalid")
+                    sum=sum+i
+                print("\nSum of all numbers from",num1,"to",num2,"is:",sum) 
+                
+
+            else:
+                for i in range(num1,num2+1):
+                    if i%2==0:
+                        print(i,"is Even")
+                    elif i%2!=0:
+                        print(i,"is Odd")
+                    elif i==0:
+                        print(i,"is Neutral")
+                    else:
+                        print("Invalid")
+                    sum=sum+i
+                print("\nSum of all numbers from",num1,"to",num2,"is:",sum) 
+
+        case 3:
+            print("\nExiting the program.Goodbye!")
+            break
+                
